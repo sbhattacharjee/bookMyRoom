9992acd328665d173601d493b182026c07c232ea
sbhattacharjee <bhattacharjee.sudarshan@gmail.com>
2014-09-26 14:55:58 +0530

    Initialize Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BookMyRoom::Application.config.secret_key_base = '379a994a6cc2501148ada2d8fc4c659ea3996535d6bd0ad62fbd147884df41987aa72bd802813d99881c091d398693f71ef574d3eb043d1625858bfa04176d8c'
